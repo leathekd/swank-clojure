38e0de7797b5c5f2c875e1f9948c6c201671d85d
David Leatherman <david@leathekd.com>
2012-03-28 08:45:42 -0400

    Added print-catches and delete-all-catches
    This cannot be applied to master until the next version of cdt is released.

--- src/swank/cdt.clj
+++ src/swank/cdt.clj
@@ -10,13 +10,21 @@
 
 (cdt/expose cbackends/set-catch cbackends/set-bp cbackends/reval
             cdt/delete-catch cdt/delete-bp cdt/delete-all-breakpoints
-            cdt/bg)
+            cdt/bg cdt/delete-all-catches)
 
 (defn print-bps []
   (swank.core/send-to-emacs
    `(:write-string
      ~(apply str "\nCDT Breakpoints:\n"
              (for [[n k] (keep-indexed vector (keys @cdt.events/bp-list))]
+               (format "%s: %s\n" n k)))
+     :repl-result)))
+
+(defn print-catches []
+  (swank.core/send-to-emacs
+   `(:write-string
+     ~(apply str "\nCDT Caught Exceptions:\n"
+             (for [[n k] (keep-indexed vector (keys @cdt.events/catch-list))]
                (format "%s: %s\n" n k)))
      :repl-result)))
 
